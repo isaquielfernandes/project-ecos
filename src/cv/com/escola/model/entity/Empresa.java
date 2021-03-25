1c97ed72ee4d3e10128ed37bf4893b149692c08f
IF <iisaquielfernandes@gmail.com>
2021-03-25 13:14:29 +0000

    removendo comentários

--- src/cv/com/escola/model/entity/Empresa.java
+++ src/cv/com/escola/model/entity/Empresa.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cv.com.escola.model.entity;
 
 import java.io.File;
@@ -18,10 +13,7 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.scene.image.Image;
 
-/**
- *
- * @author Isaquiel Fernandes
- */
+
 public class Empresa {
 
     private IntegerProperty idEscola;
--- src/cv/com/escola/model/entity/Item.java
+++ src/cv/com/escola/model/entity/Item.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cv.com.escola.model.entity;
 
 import cv.com.escola.model.entity.Artigo;
@@ -10,10 +5,6 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Objects;
 
-/**
- *
- * @author Isaquiel Fernandes
- */
 public class Item implements Serializable{
 
     private static final long serialVersionUID = 1L;
